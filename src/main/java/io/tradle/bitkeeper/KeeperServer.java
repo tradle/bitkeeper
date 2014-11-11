b2fdc2ec2baa650f4ef8a9c6e36bd67c52917ede
Mark Vayngrib <mark.vayngrib@lablz.com>
2014-11-10 23:56:14 -0800

    take in config.json path as first parameter

--- src/main/java/io/tradle/bitkeeper/KeeperServer.java
+++ src/main/java/io/tradle/bitkeeper/KeeperServer.java
@@ -22,137 +22,128 @@ import com.google.gson.*;
  * 
  */
 public class KeeperServer {
-  private static final Random RND = new Random(System.currentTimeMillis());
-  private static final String[] CONFIG_PATH = new String[]{"conf/config.json", "conf/config1.json"};
-  
-  public static void main(String[] args) throws Exception {
-    //String myIpAddress = args[0];
-    //int httpPort = Integer.parseInt(args[1]);
-    //int myDhtPort = Integer.parseInt(args[2]);
-    //int masterDhtPort = Integer.parseInt(args[3]);
-    //String masterDhtIpAddress = args[4];
-    
-    int cidx = (args == null  ||  args.length == 0) ? 0 : Integer.parseInt(args[0]) - 1;
-      
-    Config config = null;
-    
-    Gson gson = new GsonBuilder().create();
-    try {
-      config = gson.fromJson(new BufferedReader(new FileReader(CONFIG_PATH[cidx])), Config.class);
-    } catch (FileNotFoundException e) {
-      throw new IllegalStateException("couldn't find config file at path: " + CONFIG_PATH);
-    }
-    
-    String storage = config.storageDir();
-    if (storage != null) {
-      if (storage.length() > 0) {
-        File f = new File(storage);
-        if (!f.exists())
-        	f.mkdirs();
-        
-        if (!f.isDirectory())
-          throw new IllegalStateException("config parameter 'storageDir' is not a directory: " + storage);
-      }
-      else
-        storage = null;
-    }
-    
-    
-    Peer me = createOwnDhtPeer(config.address().dhtPort(), config.address().address(), storage);
-    
-    ExecutorService threadPool = Executors.newCachedThreadPool();
-    
-    // bootstrap to DHT network
-    threadPool.execute(new DhtBoostrapper(me, config.keepers().get(0).dhtPort(), config.keepers().get(0).dhtAddress()));
-    
-    // Configure the server.
-    ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
+	private static final Random RND = new Random(System.currentTimeMillis());
+	private static final String DEFAULT_CONFIG_PATH = "conf/config.json";
 
-    // Set up the event pipeline factory.
-    bootstrap.setPipelineFactory(new ServerPipelineFactory(me, threadPool));
+	public static void main(String[] args) throws Exception {
+		String configPath = args.length == 0 ? DEFAULT_CONFIG_PATH : args[0];
+		Config config = null;
+		Gson gson = new GsonBuilder().create();
+		try {
+			config = gson.fromJson(new BufferedReader(new FileReader(configPath)), Config.class);
+		} catch (FileNotFoundException e) {
+			throw new IllegalStateException("couldn't find config file at path: " + configPath);
+		}
 
-    // Bind and start to accept incoming connections.
-    bootstrap.bind(new InetSocketAddress(config.address().httpPort()));
+		String storage = config.storageDir();
+		if (storage != null) {
+			if (storage.length() > 0) {
+				File f = new File(storage);
+				if (!f.exists())
+					f.mkdirs();
 
-  }
+				if (!f.isDirectory())
+					throw new IllegalStateException("config parameter 'storageDir' is not a directory: " + storage);
+			} 
+			else
+				storage = null;
+		}
 
-  public static Peer createOwnDhtPeer(int myPort, String myIp, String storageDir) throws Exception {
-    InetAddress myIpAddrress = Inet4Address.getByName(myIp);
-    Bindings b = new Bindings(Protocol.IPv4, myIpAddrress, myPort, myPort);
-    // b.addInterface("eth0");
-    Peer client = new PeerMaker(new Number160(RND))
-                  .setPorts(myPort)
-                  .setEnableIndirectReplication(true)
-                  .setStorage(storageDir != null? new StorageDisk(storageDir) : new StorageMemory())
-                  .setBindings(b)
-                  .makeAndListen();
-    System.out.println("DHT client started and Listening to: " + DiscoverNetworks.discoverInterfaces(b));
-    System.out.println("DHT client address visible to outside is " + client.getPeerAddress());
-    return client;
-  }
-  
-  static class DhtBoostrapper implements Runnable {
-    int masterPort;
-    InetAddress masterAddress;
-    PeerAddress pa;
-    Peer myself;
-    boolean bootstrapped;
-    
-    DhtBoostrapper(Peer myself, int masterPort, String masterIp) throws Exception {
-      masterAddress = Inet4Address.getByName(masterIp);
-      this.masterPort = masterPort;
-      pa = new PeerAddress(Number160.ZERO, masterAddress, masterPort, masterPort);
-      this.myself = myself;
-    }
+		Peer me = createOwnDhtPeer(config.address().dhtPort(), config.address().address(), storage);
 
-    public void run() {
-      while (true) {
-        if (!bootstrapped) {
-          // Future Bootstrap - slave
-          FutureBootstrap futureBootstrap = myself.bootstrap().setInetAddress(masterAddress).setPorts(masterPort).start();
-          futureBootstrap.awaitUninterruptibly();
-          if (futureBootstrap.isSuccess()) {
-            System.out.println("succeded to bootstrap to DHC");
-            bootstrapped = true;
-          } else {
-            System.out.println("failed " + futureBootstrap.getFailedReason());
-            bootstrapped = false;
-          }
-        }
-        else {
-          // ping
-          FutureChannelCreator fcc = myself.getConnectionBean().getConnectionReservation().reserve( 1 );
-          fcc.awaitUninterruptibly();
+		ExecutorService threadPool = Executors.newCachedThreadPool();
 
-          ChannelCreator cc = fcc.getChannelCreator();
+		// bootstrap to DHT network
+		threadPool.execute(new DhtBoostrapper(me, config.keepers().get(0).dhtPort(), config.keepers().get(0).dhtAddress()));
 
-          FutureResponse fr1 = myself.getHandshakeRPC().pingTCP(pa, cc);
-          fr1.awaitUninterruptibly();
+		// Configure the server.
+		ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
 
-          if (fr1.isSuccess()) {
-            //System.out.println("peer online T:" + pa);
-          }  
-          else {
-            System.out.println("peer offline " + pa);
-            bootstrapped = false;
-          } 
-          // FutureResponse fr2 = master.getHandshakeRPC().pingUDP(pa, cc);
-          // fr2.awaitUninterruptibly();
+		// Set up the event pipeline factory.
+		bootstrap.setPipelineFactory(new ServerPipelineFactory(me, threadPool));
 
-          myself.getConnectionBean().getConnectionReservation().release(cc);
+		// Bind and start to accept incoming connections.
+		bootstrap.bind(new InetSocketAddress(config.address().httpPort()));
 
-          // if (fr2.isSuccess())
-          // System.out.println("peer online U:" + pa);
-          // else
-          // System.out.println("offline " + pa);
+	}
 
-        }
-        try {
-          Thread.sleep(3000);
-        } catch (InterruptedException e) {
-          break;
-        }
-      }  
-    }
-  }
+	public static Peer createOwnDhtPeer(int myPort, String myIp,
+			String storageDir) throws Exception {
+		InetAddress myIpAddrress = Inet4Address.getByName(myIp);
+		Bindings b = new Bindings(Protocol.IPv4, myIpAddrress, myPort, myPort);
+		// b.addInterface("eth0");
+		Peer client = new PeerMaker(new Number160(RND))
+				.setPorts(myPort)
+				.setEnableIndirectReplication(true)
+				.setStorage(storageDir != null ? new StorageDisk(storageDir) : new StorageMemory()).setBindings(b)
+				.makeAndListen();
+		
+		System.out.println("DHT client started and Listening to: " + DiscoverNetworks.discoverInterfaces(b));
+		System.out.println("DHT client address visible to outside is " + client.getPeerAddress());
+		return client;
+	}
+
+	static class DhtBoostrapper implements Runnable {
+		int masterPort;
+		InetAddress masterAddress;
+		PeerAddress pa;
+		Peer myself;
+		boolean bootstrapped;
+
+		DhtBoostrapper(Peer myself, int masterPort, String masterIp) throws Exception {
+			masterAddress = Inet4Address.getByName(masterIp);
+			this.masterPort = masterPort;
+			pa = new PeerAddress(Number160.ZERO, masterAddress, masterPort, masterPort);
+			this.myself = myself;
+		}
+
+		public void run() {
+			while (true) {
+				if (!bootstrapped) {
+					// Future Bootstrap - slave
+					FutureBootstrap futureBootstrap = myself.bootstrap().setInetAddress(masterAddress).setPorts(masterPort).start();
+					futureBootstrap.awaitUninterruptibly();
+					if (futureBootstrap.isSuccess()) {
+						System.out.println("succeded to bootstrap to DHC");
+						bootstrapped = true;
+					} else {
+						System.out.println("failed " + futureBootstrap.getFailedReason());
+						bootstrapped = false;
+					}
+				} else {
+					// ping
+					FutureChannelCreator fcc = myself.getConnectionBean().getConnectionReservation().reserve(1);
+					fcc.awaitUninterruptibly();
+
+					ChannelCreator cc = fcc.getChannelCreator();
+
+					FutureResponse fr1 = myself.getHandshakeRPC().pingTCP(pa, cc);
+					fr1.awaitUninterruptibly();
+
+					if (fr1.isSuccess()) {
+						// System.out.println("peer online T:" + pa);
+					} else {
+						System.out.println("peer offline " + pa);
+						bootstrapped = false;
+					}
+					// FutureResponse fr2 = master.getHandshakeRPC().pingUDP(pa,
+					// cc);
+					// fr2.awaitUninterruptibly();
+
+					myself.getConnectionBean().getConnectionReservation().release(cc);
+
+					// if (fr2.isSuccess())
+					// System.out.println("peer online U:" + pa);
+					// else
+					// System.out.println("offline " + pa);
+
+				}
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					break;
+				}
+			}
+		}
+	}
 }
